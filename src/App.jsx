7fbfd250bd3232a484f65329dc7c1294e392d71f
Saswata Paul <saspaul2014@gmail.com>
2024-03-17 19:10:36 +0530

    added the basic structure of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import TitleSection from "./Components/TitleSection"
 import GenerationSection from "./Components/GenerationSection"
 import InformationSection from "./Components/InformationSection"
+import "./Components/Style/app.css"
 
 function App() {
 
   return (
     <div>
       <TitleSection />
-      <InformationSection />
-      <GenerationSection />
+      <div className="home-section">
+        <InformationSection />
+        <GenerationSection />
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
--- src/Components/TitleSection.jsx
+++ src/Components/TitleSection.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import './Style/titleSection.css'
 
 
 const TitleSection = () => {
   return (
-    <div className='title_container'>
-      <div className='title-section'>
+    <div className="title_container">
+      <div className="title-section">
+        <img 
+          src="/public/Assests/icons/cv.png" 
+          height={60} 
+        />
         <h2>CV GENERATOR</h2>
       </div>
-      <div className='github-link'>
-
+      <div className="github-link">
+        <a href="https://github.com/Paul-iwnl/CV_application" target="_blank">
+          <img 
+            src="/public/Assests/icons/github-mark.svg" 
+            height={40}
+          />
+        </a>
       </div>
     </div>
-  )
+  );
 }
 
 export default TitleSection
